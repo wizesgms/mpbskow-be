0fd9907dab3bf1413f7333dc6ff0fb1a1d028bcf
wizesgms <4uklyzz@gmail.com>
2024-06-22 14:26:08 +0700

    update b2

--- resources/views/deposit/pending.blade.php
+++ resources/views/deposit/pending.blade.php
@@ -54,7 +54,7 @@
                                         {{ $trx->metode }}</td>
                                     <td class="text-center"
                                         style="vertical-align: middle; white-space: normal; font-size: 13px;">
-                                        {{ $trx->bonus_amount }}</td>
+                                        {{ $trx->bonus_amount }}%</td>
                                     <td class="text-center"
                                         style="vertical-align: middle; white-space: normal; font-size: 13px;">
                                         {{ $trx->keterangan }}</td>
